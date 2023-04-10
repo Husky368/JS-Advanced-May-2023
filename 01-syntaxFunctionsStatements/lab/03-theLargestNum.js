c92d2f3ee6021a0a3f1469518091aa716456707d
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-04-10 23:06:09 +0300

    Create 03-theLargestNum.js

---
+++ 01-syntaxFunctionsStatements/lab/03-theLargestNum.js
@@ -0,0 +1,5 @@
+function theLargestNum(first,second,third){
+    let largest = Math.max(first,second,third)
+    console.log(`The largest number is ${largest}.`)
+    //`The largest number is {number}.`.
+}theLargestNum(5, -3, 16)
