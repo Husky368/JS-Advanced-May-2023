91ef8f52aa651b9eff3b3fc182d6283163929abd
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-04-10 23:05:55 +0300

    Create 02-stringLength.js

---
+++ 01-syntaxFunctionsStatements/lab/02-stringLength.js
@@ -0,0 +1,5 @@
+function stringLength(first,second,thrird){
+    let sum = first.length+second.length+thrird.length
+    console.log(sum)
+    console.log(Math.floor(sum/3))
+}stringLength('pasta', '5', '22.3')
