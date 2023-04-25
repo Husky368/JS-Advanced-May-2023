51e3f65f6fc04ed1cdca10bd0298309f1b76656f
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-04-25 23:04:16 +0300

    Update 11-equalNeightboars.js

--- 02-arraysAndNestedArrays/lab/11-equalNeightboars.js
+++ 02-arraysAndNestedArrays/lab/11-equalNeightboars.js
@@ -1,19 +1,14 @@
 function equalNeightboars(arr) {
     let pairs = 0;
-    for (let index = 0; index < arr.length - 1; index++) {
-        //['2', '3', '4', '7', '0']
-        if (index == 0) {
-            for (let innerIndex = 0; innerIndex < arr[index].length; innerIndex++) {
-                //'2'
-                //'3'
-                //'4',
-                //7'
-                //'0
+    for (let index = 0; index < arr.length; index++) {
+        for (let innerIndex = 0; innerIndex < arr[index].length; innerIndex++) {
+            if (index != arr.length - 1 && arr[index][innerIndex] == arr[index + 1][innerIndex]) {
+                pairs++
+            }
+            if (arr[index][innerIndex] == arr[index][innerIndex - 1]) {
+                pairs++
             }
         }
     }
-} equalNeightboars(
-    [['2', '3', '4', '7', '0'],
-    ['4', '0', '5', '3', '4'],
-    ['2', '3', '5', '4', '2'],
-    ['9', '8', '7', '5', '4']])
+    return console.log(pairs)
+} 
