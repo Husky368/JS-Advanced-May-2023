7f82888a34f7f1f7fa90d3873f2810b604067b6b
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-05-02 23:21:48 +0300

    Update 03-addAndRemove.js

--- 02-arraysAndNestedArrays/exercises/03-addAndRemove.js
+++ 02-arraysAndNestedArrays/exercises/03-addAndRemove.js
@@ -1,7 +1,21 @@
-function addAndRemove(arr){
+function addAndRemove(arr) {
     let resArr = [];
-}addAndRemove(['add',
-'add',
-'add',
-'add']
-)
+    let addExist = false;
+    let number = 0;
+    for (const el of arr) {
+        number++;
+        if (el == 'add') {
+            addExist = true;
+            resArr.push(number);
+        }
+        else if (el == 'remove') {
+            resArr.pop();
+        }
+    }
+    if (!addExist || resArr.length == 0) {
+        console.log('Empty');
+    }
+    else {
+        return console.log(resArr.join('\n'))
+    }
+}
