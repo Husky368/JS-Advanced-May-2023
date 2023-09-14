8db451ceaf6a258276d936cc546c3110aa918b48
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-09-14 14:27:38 +0300

    Update 01-evenPositions.js

--- 02-arraysAndNestedArrays/lab/01-evenPositions.js
+++ 02-arraysAndNestedArrays/lab/01-evenPositions.js
@@ -1,9 +1,9 @@
-function evenPositions(arr) {
-    let resArr = []
+function evenPositionEl(arr){
+    let newArr = [];
     for (let index = 0; index < arr.length; index++) {
-        if (index % 2 == 0) {
-        resArr.push(arr[index])
+        if(index%2==0){
+            newArr.push(arr[index])
         }
     }
-    console.log(resArr.join(' '))
-} evenPositions(['20', '30', '40', '50', '60'])
+    console.log(newArr.join(" "))
+}
