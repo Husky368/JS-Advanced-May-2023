5f794a97905020bd3a5fba46bb24540fb3c47657
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-09-20 22:48:02 +0300

    Create 01-cityRecord.js

---
+++ 03-objectsAndComposition/01-cityRecord.js
@@ -0,0 +1,8 @@
+function cityRecord(nameInfo,populationInfo,treasuryInfo){
+    let object = {
+        name: nameInfo,
+        population : populationInfo,
+        treasury: treasuryInfo
+    }; 
+    return object
+}
