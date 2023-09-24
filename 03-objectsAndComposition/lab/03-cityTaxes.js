8175c8b3fe91a88f3ce8536e1002df9f15937930
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-09-24 23:03:09 +0300

    Update 03-cityTaxes.js

--- 03-objectsAndComposition/lab/03-cityTaxes.js
+++ 03-objectsAndComposition/lab/03-cityTaxes.js
@@ -1,1 +1,18 @@
-asd
+function cityTaxes(nameInfo, populationInfo, treasuryInfo) {
+    let object = {
+        name: nameInfo,
+        population: populationInfo,
+        treasury: treasuryInfo,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += this.population * this.taxRate;
+        },
+        applyGrowth(percent) {
+            this.population += (this.population*percent/100)
+        },
+        applyRecession(percent) {
+            this.treasury -= this.treasury*percent/100
+        }
+    };
+    return object
+}
