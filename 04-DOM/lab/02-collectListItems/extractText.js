da4e715321220d8bd080b592064b5a2cfa10a29d
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-10-04 23:30:43 +0300

    Create extractText.js

---
+++ 04-DOM/lab/02-collectListItems/extractText.js
@@ -0,0 +1,9 @@
+function extractText() {
+    // TODO
+    let lis = document.getElementsByTagName('li');
+    let liArray = Array.from(lis);
+    let allTexts = liArray.map( x => x.textContent);
+    
+    let textArea = document.getElementById('result');
+    textArea.value = allTexts.join("\n");
+}
