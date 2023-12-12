3155e8bf71dd66036a336aec425744afe0dcaaeb
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-12-12 22:41:09 +0200

    Update 02-playingCards.js

--- 09-unitTesting&errorHandling/lab/02-playingCards.js
+++ 09-unitTesting&errorHandling/lab/02-playingCards.js
@@ -1,30 +1,25 @@
 function playingCards(faceInput, suitInput) {
     let errorText = 'Error';
-    let facesArr = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+    let facesArr = ['2', '3', '4', '5', '6', '7', '8', '9', '10','J', 'Q', 'K', 'A'];
     let suitsArr = ["S", "H", "D", "C"];
-    if (!facesArr.includes(faceInput)) {
-        return errorText;
+
+    if( !facesArr.includes(faceInput)){
+        return errorText.toString();
     }
     if (suitInput == 'S') {
         let result = `${faceInput}\u2660`;
-        return result
+        return result.toString()
     }
     else if (suitInput == 'H') {
         let result = `${faceInput}\u2665`;
-        return result
+        return result.toString()
     }
     else if (suitInput == 'D') {
         let result = `${faceInput}\u2666`;
-        return result
+        return result.toString()
     }
     else if (suitInput == 'D') {
         let result = `${faceInput}\u2663`;
-        return result
+        return result.toString()
     }
-} console.log(playingCards('1', 'C'))
-//  Valid card faces are: 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
-//  Valid card suits are: S (♠), H (♥), D (♦), C (♣)
-// \u2660 – Spades (♠)
-//  \u2665 – Hearts (♥)
-//  \u2666 – Diamonds (♦)
-//  \u2663 – Clubs (♣)
+}
