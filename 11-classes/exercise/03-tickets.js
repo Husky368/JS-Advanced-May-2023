a68570246ace0fc019d5a128b77687272018bad7
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2024-01-10 12:45:07 +0200

    Update 03-tickets.js

--- 11-classes/exercise/03-tickets.js
+++ 11-classes/exercise/03-tickets.js
@@ -1,28 +1,30 @@
-class Ticket {
+function tickets(destinationsInfoArr, sortBy) {
+    class Ticket {
     constructor(_destinationInfo) {
-        this.destinationTicket = _destinationInfo[0];
-        this.priceTicket = _destinationInfo[1];
-        this.statusTicket = _destinationInfo[2];
+        this.destination = _destinationInfo[0];
+        this.price = Number(_destinationInfo[1]);
+        this.status = _destinationInfo[2];
+        }
     }
-}
-let splittedArr = [];
-let sortedArr = [];
-let resultArr = [];
+    let splittedArr = [];
+    let sortedArr = [];
+    let resultArr = [];
 
-for (let destinationInfo of destinationsInfoArr) {
-    let [destination, price, status] = destinationInfo.split('|');
-    splittedArr.push([destination, price, status])
-}
-if (sortBy == 'destination') {
-    sortedArr = splittedArr.sort((a, b) => a[0].localeCompare(b[0]));
-} else if (sortBy == 'price') {
-    sortedArr = splittedArr.sort((a, b) => Number(a[1]) - Number(b[1]));
-} else {
-    sortedArr = splittedArr.sort((a, b) => a[2].localeCompare(b[2]));
-}
+    for (let destinationInfo of destinationsInfoArr) {
+        let [destination, price, status] = destinationInfo.split('|');
+        splittedArr.push([destination, price, status])
+    }
+    if (sortBy == 'destination') {
+        sortedArr = splittedArr.sort((a, b) => a[0].localeCompare(b[0]));
+    } else if (sortBy == 'price') {
+        sortedArr = splittedArr.sort((a, b) => Number(a[1]) - Number(b[1]));
+    } else {
+        sortedArr = splittedArr.sort((a, b) => a[2].localeCompare(b[2]));
+    }
 
-for (let destinationInfo of sortedArr) {
-    let obj = new Ticket(destinationInfo)
-    resultArr.push(obj)
+    for (let destinationInfo of sortedArr) {
+        let obj = new Ticket(destinationInfo)
+        resultArr.push(obj)
+    }
+    return resultArr
 }
-return resultArr
