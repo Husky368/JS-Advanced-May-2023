78371d959593429e64e3c0ecba1d1a5839ca0d36
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2024-01-24 22:22:19 +0200

    Update 01-subSum.js

--- 09-unitTesting&errorHandling/lab/01-subSum.js
+++ 09-unitTesting&errorHandling/lab/01-subSum.js
@@ -1,22 +1,31 @@
-function subSum(array, start, end) {
-    if (!Array.isArray(array)) {
+// Write a function to sum a range of numeric elements from an array.
+// The function takes three parameters - the first is an array, the second is the start index and the third is the end
+// index. Both indexes are inclusive. Have in mind that the array elements may not be of type Number and cast
+// everything. Implement the following error handling:
+//  If the first element is not an array, return NaN
+//  If the start index is less than zero, consider its value to be a zero
+//  If the end index is outside the bounds of the array, assume it points to the last index of the array
+// Input / Output[10, 20, 30, 40, 50, 60], 3, 300
+// Your function must take three parameters. As output, return the sum.
+function subSum(inputArr, inputStart, inputEnd) {
+    if (Array.isArray(inputArr) === false) {
         return NaN
     }
-    for (const el of array) {
-        let elType = typeof(el);
-        if(elType!='number'){
-            return NaN
-        }
+    if (inputStart < 0) {
+        inputStart = 0;
     }
-    if (start < 0) {
-        start = 0
+    if (inputEnd > inputArr.length - 1) {
+        inputEnd = inputArr.length - 1;
     }
-    if (end > array.length - 1) {
-        end = array.length - 1
+    let result = 0;
+    let isAllNumber = inputArr.some((a) => typeof a == Number)
+    if(!isAllNumber){
+        return NaN
     }
-    let sum = 0;
-    for (let index = start; index <= end; index++) {
-        sum += array[index]
+    for (let index = inputStart; index <= inputEnd; index++) {
+        result += inputArr[index]
     }
-    return sum
+    return result
 }
+let a = subSum([], 1, 2)
+console.log(a)
